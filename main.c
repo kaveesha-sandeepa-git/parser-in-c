d79eac7a03d39db2c41ddf314c1a529a0fcbe634
kaveesha <sandeepakaveesha957@gmail.com>
2026-02-07 20:05:44 +0530

    continuing the lexer

--- main.c
+++ main.c
@@ -7,15 +7,37 @@ typedef enum
 {
     INT,
     KEYWORD,
-    SEPARATOR
+    SEPARATOR,
+    END_OF_TOKEN
 } TokenType;
-
 
 typedef struct
 {
     TokenType type;
     char *value;
 } Token;
+
+void print_token(Token token)
+{
+    for (int i = 0; token.value[i] != '\0'; i++)
+    {
+        printf("%c", token.value[i]);
+    }
+    printf("\n");
+
+    if (token.type == INT)
+    {
+        printf("TOKEN TYPE:INT ");
+    }
+    if (token.type == KEYWORD)
+    {
+        printf("TOKEN TYPE:KEYWORD ");
+    }
+    if (token.type == SEPARATOR)
+    {
+        printf("TOKEN TYPE:SEPARATOR ");
+    }
+}
 
 // TokenLiteral generate_number(char current, int curr_index)
 // {
@@ -43,46 +65,52 @@ typedef struct
 //     return (token);
 // }
 
-Token *generate_number(char *current,int *curr_index){
+Token *generate_number(char *current, int *curr_index)
+{
     Token *token = malloc(sizeof(Token));
     token->type = INT;
-    char *value =malloc(sizeof(char) * 8);
-    int value_index =0;
-    while(isdigit(current[*curr_index]) && current[*curr_index] != '\0'){
+    char *value = malloc(sizeof(char) * 8);
+    int value_index = 0;
+    while (isdigit(current[*curr_index]) && current[*curr_index] != '\0')
+    {
         value[value_index] = current[*curr_index];
-        //value = value *10 + (current[*curr_index] - '0');
+        // value = value *10 + (current[*curr_index] - '0');
         value_index++;
-       
+
         *curr_index += 1;
     }
     value[value_index] = '\0';
     token->value = value;
-    //free(value);
+    // free(value);
     return token;
 }
 
-Token *generate_keyword(char *current,int *curr_index){
+Token *generate_keyword(char *current, int *curr_index)
+{
     Token *token = malloc(sizeof(Token));
     char *keyword = malloc(sizeof(char) * 9);
     int keyword_index = 0;
 
-    while(isalpha(current[*curr_index]) && current[*curr_index] != '\0'){
+    while (isalpha(current[*curr_index]) && current[*curr_index] != '\0')
+    {
         keyword[keyword_index] = current[*curr_index];
         keyword_index++;
         *curr_index += 1;
-
     }
     keyword[keyword_index] = '\0';
     // printf("keyword index %d",keyword_index);
     // for(int i=0;i<keyword_index;i++){
     //     printf("%c\n",keyword[i]);
     // }
-    if(strcmp(keyword,"exit") ==0){
-        printf("type EXIT\n");
+    if (strcmp(keyword, "exit") == 0)
+    {
+        //printf("type EXIT\n");
         token->type = KEYWORD;
+        // token->value = malloc(strlen(keyword) + 1);
         token->value = "EXIT";
     }
-    return token; 
+    free(keyword);
+    return token;
 }
 // void lexer(FILE *file)
 // {
@@ -119,52 +147,96 @@ Token *generate_keyword(char *current,int *curr_index){
 //     printf("\n");
 // }
 
-void lexer(FILE *file){
+size_t tokens_index;
+
+Token *lexer(FILE *file)
+{
     int length;
     char *buffer = 0;
-    fseek(file,0,SEEK_END);
+    fseek(file, 0, SEEK_END);
     length = ftell(file);
-    fseek(file,0,SEEK_SET);
-    buffer = malloc(sizeof(char) * length +1);
-    fread(buffer,1,length,file);
-    //fclose(file);
+    fseek(file, 0, SEEK_SET);
+    buffer = malloc(sizeof(char) * length + 1);
+    fread(buffer, 1, length, file);
+    // fclose(file);
     buffer[length] = '\0';
-    //char *current = malloc(sizeof(char) * length +1);
+    // char *current = malloc(sizeof(char) * length +1);
     char *current = buffer;
     int curr_index = 0;
-    
-    while(current[curr_index] != '\0'){
+    Token *tokens = malloc(sizeof(Token) * 1024);
+    tokens_index = 0;
+
+    while (current[curr_index] != '\0')
+    {
         if (current[curr_index] == ';')
         {
-            printf("FOUND SEMI COLUMN\n");
+            Token *token = malloc(sizeof(Token));
+            token->value = malloc(2);
+            token->value[0] = current[curr_index];
+            token->value[1] = '\0';
+            token->type = SEPARATOR;
+            tokens[tokens_index] = *token;
+            tokens_index++;
+            print_token(*token);
+            //printf("FOUND SEMI COLUMN\n");
+            free(token->value);
+            free(token);
         }
         else if (current[curr_index] == '(')
         {
-            printf("FOUND OPEN_PARAN\n");
+            Token *token = malloc(sizeof(Token));
+            token->value = malloc(2);
+            token->value[0] = current[curr_index];
+            token->value[1] = '\0';
+            token->type = SEPARATOR;
+            tokens[tokens_index] = *token;
+            tokens_index++;
+            print_token(*token);
+            //printf("FOUND OPEN_PARAN\n");
+            free(token->value);
+            free(token);
         }
         else if (current[curr_index] == ')')
         {
-            printf("FOUND CLOSE_PARAN\n");
+            Token *token = malloc(sizeof(Token));
+            token->value = malloc(2);
+            token->value[0] = current[curr_index];
+            token->value[1] = '\0';
+            token->type = SEPARATOR;
+            tokens[tokens_index] = *token;
+            tokens_index++;
+            print_token(*token);
+           // printf("FOUND CLOSE_PARAN\n");
+            free(token->value);
+            free(token);
         }
         else if (isdigit(current[curr_index]))
         {
-            Token *test_token = generate_number(current,&curr_index);
+            Token *int_token = generate_number(current, &curr_index);
+            print_token(*int_token);
             curr_index--; // adjust index after number parsing
-            printf("Test token value : %s\n",test_token->value);
+            printf(" \ntoken value : %s\n", int_token->value);
+            free(int_token->value);
+            free(int_token);
         }
         else if (isalpha(current[curr_index]))
         {
-            Token *test_keyword = generate_keyword(current,&curr_index);
+            Token *keyword_token = generate_keyword(current, &curr_index);
             curr_index--; // adjust index after keyword parsing
-            //printf("test keyword %s\n", test_keyword->keyword);
-            printf("current char:%c\n",current[curr_index]);
+            // printf("test keyword %s\n", test_keyword->keyword);
+            print_token(*keyword_token);
+            //printf("current char:%c\n", current[curr_index]);
+            // free(test_keyword->value);
+            free(keyword_token);
         }
         curr_index++;
     }
     free(buffer);
-    //free(current);
-    //free(test_token->value);
-    //free(test_token);
+    tokens[tokens_index].value = '\0';
+    return tokens;
+    // free(current);
+    // free(test_token->value);
+    // free(test_token);
 }
 
 int main()
