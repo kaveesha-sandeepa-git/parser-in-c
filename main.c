ae0f2e44601a605f66913860f4dcdb1cd2b5aaef
kaveesha <sandeepakaveesha957@gmail.com>
2026-02-08 08:20:42 +0530

    finished the lexer

--- main.c
+++ main.c
@@ -19,23 +19,28 @@ typedef struct
 
 void print_token(Token token)
 {
+    printf("TOKEN VALUE: ");
     for (int i = 0; token.value[i] != '\0'; i++)
     {
         printf("%c", token.value[i]);
     }
-    printf("\n");
 
-    if (token.type == INT)
+    switch (token.type)
     {
-        printf("TOKEN TYPE:INT ");
-    }
-    if (token.type == KEYWORD)
-    {
-        printf("TOKEN TYPE:KEYWORD ");
-    }
-    if (token.type == SEPARATOR)
-    {
-        printf("TOKEN TYPE:SEPARATOR ");
+    case INT:
+        printf(" TOKEN TYPE :INT\n");
+        break;
+
+    case KEYWORD:
+        printf(" TOKEN TYPE :KEYWORD\n");
+        break;
+    case SEPARATOR:
+        printf(" TOKEN TYPE :SEPARATOR\n");
+        break;
+
+    case END_OF_TOKEN:
+        printf(" END_OF_TOKEN\n");
+        break;
     }
 }
 
@@ -104,7 +109,7 @@ Token *generate_keyword(char *current, int *curr_index)
     // }
     if (strcmp(keyword, "exit") == 0)
     {
-        //printf("type EXIT\n");
+        // printf("type EXIT\n");
         token->type = KEYWORD;
         // token->value = malloc(strlen(keyword) + 1);
         token->value = "EXIT";
@@ -147,6 +152,15 @@ Token *generate_keyword(char *current, int *curr_index)
 //     printf("\n");
 // }
 
+Token *generate_separator(char *current, int *curr_index)
+{
+    Token *token = malloc(sizeof(Token));
+    token->value = malloc(2);
+    token->value[0] = current[*curr_index];
+    token->value[1] = '\0';
+    token->type = SEPARATOR;
+    return token;
+}
 size_t tokens_index;
 
 Token *lexer(FILE *file)
@@ -163,50 +177,39 @@ Token *lexer(FILE *file)
     // char *current = malloc(sizeof(char) * length +1);
     char *current = buffer;
     int curr_index = 0;
-    Token *tokens = malloc(sizeof(Token) * 1024);
+     Token *tokens = malloc(sizeof(Token) * 1024);
     tokens_index = 0;
 
     while (current[curr_index] != '\0')
     {
+        Token *token = malloc(sizeof(Token));
         if (current[curr_index] == ';')
         {
-            Token *token = malloc(sizeof(Token));
-            token->value = malloc(2);
-            token->value[0] = current[curr_index];
-            token->value[1] = '\0';
-            token->type = SEPARATOR;
+            token = generate_separator(current,&curr_index);
             tokens[tokens_index] = *token;
             tokens_index++;
             print_token(*token);
-            //printf("FOUND SEMI COLUMN\n");
+            // printf("FOUND SEMI COLUMN\n");
             free(token->value);
             free(token);
         }
         else if (current[curr_index] == '(')
         {
-            Token *token = malloc(sizeof(Token));
-            token->value = malloc(2);
-            token->value[0] = current[curr_index];
-            token->value[1] = '\0';
-            token->type = SEPARATOR;
+            token = generate_separator(current,&curr_index);
             tokens[tokens_index] = *token;
             tokens_index++;
             print_token(*token);
-            //printf("FOUND OPEN_PARAN\n");
+            // printf("FOUND OPEN_PARAN\n");
             free(token->value);
             free(token);
         }
         else if (current[curr_index] == ')')
         {
-            Token *token = malloc(sizeof(Token));
-            token->value = malloc(2);
-            token->value[0] = current[curr_index];
-            token->value[1] = '\0';
-            token->type = SEPARATOR;
+            token = generate_separator(current,&curr_index);
             tokens[tokens_index] = *token;
             tokens_index++;
             print_token(*token);
-           // printf("FOUND CLOSE_PARAN\n");
+            // printf("FOUND CLOSE_PARAN\n");
             free(token->value);
             free(token);
         }
@@ -225,9 +228,16 @@ Token *lexer(FILE *file)
             curr_index--; // adjust index after keyword parsing
             // printf("test keyword %s\n", test_keyword->keyword);
             print_token(*keyword_token);
-            //printf("current char:%c\n", current[curr_index]);
-            // free(test_keyword->value);
+            // printf("current char:%c\n", current[curr_index]);
+            //  free(test_keyword->value);
             free(keyword_token);
+        }
+        else if(current[curr_index] == ' '){
+            continue;
+        }
+        else{
+            printf("ERROR UNKNOWN CHARACTER\n"); 
+            exit(1);
         }
         curr_index++;
     }
@@ -239,10 +249,16 @@ Token *lexer(FILE *file)
     // free(test_token);
 }
 
-int main()
+
+
+int main(int argc,char *argv[])
 {
+    if(argc < 2){
+        printf("usage : %s <file name> \n",argv[0]);
+        exit(1);
+    }
     FILE *file;
-    file = fopen("test.unn", "r");
+    file = fopen(argv[1], "r");
     if (!file)
     {
         perror("Failed to open file");
